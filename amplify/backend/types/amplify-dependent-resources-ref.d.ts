a3ce878c3cc460a93bfbad4c94526862a3ab2907
yamamiiinsagamiiin <yamamiiinsagamiiin@gmail.com>
2025-09-09 08:11:41 +0900

    amplify set info

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "api4af33d4c": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "awsconteste3308e42": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
